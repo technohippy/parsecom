7e3f4126261150b663454c243bbff02019c36414
Ando Yasushi <andyjpn@gmail.com>
2013-10-08 22:19:34 +0900

    やっちゃった

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,21 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.allow_http_connections_when_no_cassette = true
+  c.filter_sensitive_data("<COOKIE-KEY>") { |i| [i.response.headers['Set-Cookie']].flatten.compact.first }
+
+  def filter_sensitive_header(c, header)
+    c.filter_sensitive_data("<#{header}>") do |interaction|
+      if v = interaction.request.headers.detect{|k,_| k.casecmp(header) == 0}
+        v.last.first
+      end
+    end
+  end
+
+  filter_sensitive_header(c, 'X-Parse-Application-Id')
+  filter_sensitive_header(c, 'X-Parse-Rest-Api-Key')
+  filter_sensitive_header(c, 'X-Parse-REST-API-Key')
+  filter_sensitive_header(c, 'X-Parse-Master-Key')
+  filter_sensitive_header(c, 'X-Parse-Session-Token')
 end
 
 require 'parsecom'
